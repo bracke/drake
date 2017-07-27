fa08f6e0e9055986b0cfa77492412b903509fb89
Bent Bracke <bent@bracke.dk>
2017-07-27 20:39:37 +0200

    Reverted various settings to glimmer default.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import App from './main';
 import { ComponentManager, setPropertyDidChange } from '@glimmer/component';
-import initializeCustomElements from '@glimmer/web-component';
 
 const app = new App();
 const containerElement = document.getElementById('app');
@@ -18,4 +17,3 @@ app.registerInitializer({
 app.renderComponent('drake-app', containerElement, null);
 
 app.boot();
-initializeCustomElements(app, ['drake-app']);
